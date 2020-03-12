bbdc7f50f656632d94d6b0d71bebbda2c2a26d8d
inbot <liwenrui@inbot.cn>
2020-03-12 17:07:43 +0800

    Updates

---
+++ precache-manifest.18e785247a27158a8138709d626316de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4964b51daa13799a2b42e0706e586",
+    "url": "/reactDemo01/index.html"
+  },
+  {
+    "revision": "8ac3f6a44e5b0fe2db37",
+    "url": "/reactDemo01/static/js/2.69708c80.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactDemo01/static/js/2.69708c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76145b472fdbfe6de818",
+    "url": "/reactDemo01/static/js/main.f476236a.chunk.js"
+  },
+  {
+    "revision": "e58a059beb5ee1eb9abb",
+    "url": "/reactDemo01/static/js/runtime-main.0ec40b9c.js"
+  }
+]);
